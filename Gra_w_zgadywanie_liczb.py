438a0a0faa3be60f614ca6c2214005b418f18d41
e-dit <edyta-cyran20@wp.pl>
2021-02-13 13:42:33 +0100

    gra w zgadywanie liczb z readme

--- Gra_w_zgadywanie_liczb.py
+++ Gra_w_zgadywanie_liczb.py
@@ -1,6 +1,11 @@
 import random
 
-def get_number():  # funkcja pobiera liczbę, którą wskazał użytkownik
+def get_number():
+    """ Funkcja pobiera liczbę, którą wskazał użytkownik.
+        Pyta dopóki nie otrzyma poprawnej liczby.
+        :rtype: int
+        :return: pobrana liczba jako int
+    """
     while True:
         try:
             result = int(input("Guess the number: "))
@@ -10,8 +15,11 @@ def get_number():  # funkcja pobiera liczbę, którą wskazał użytkownik
     return result
 
 def get_computer_number():
-    """Main function with our game."""
-    computer_number = random.randint(1, 10)
+    """ Main function with our game.
+        Główna funkcja losuje liczbę w zakresie 1 – 100
+        oraz zwraca informację czy liczba jest za mała, za duża czy równa wylosowanej.
+    """
+    computer_number = random.randint(1, 100)
     guess_number = get_number()
     while guess_number != computer_number:
         if guess_number < computer_number:
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Gra-w-zgadywanie-liczb
+
+Komputer losuje liczbę w zakresie 1 – 100. 
+Następnie pyta użytkownika: "Guess the number: " pobiera liczbę z klawiatury, a następnie sprawdza 
+czy wprowadzony napis jest liczbą. W przypadku błędu wyświetlia komunikat "It's not a number!".
+Użytkownik ma za zadanie zgadnąć jaką liczbe wylosował komputer.
+Jeżeli podana przez użytkownika liczba jest mniejsza niż wylosowana, wyświetla komunikat "To small!".
+Użytkownik ponownie podaje swój typ. Jeżeli podana przez użytkownika liczba jest większa niż wylosowana, 
+wyświetla komunikat "To big!". Użytkownik ponownie podaje swój typ.
+Jeśli liczba podana przez użytkownika jest równa wylosowanej, wyświetla komunikat "You win!"i kończy działanie programu.
